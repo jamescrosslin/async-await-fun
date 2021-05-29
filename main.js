12608fd0ce65064159fc013c0e118baa36a24bb5
James Crosslin <jamesmscrosslin@gmail.com>
2021-05-29 14:47:11 -0700

    switched to async/await syntax

--- main.js
+++ main.js
@@ -9,23 +9,35 @@ const parseJson = (response) => {
   return response.json();
 };
 
-function getGarchomp() {
+async function getGarchomp() {
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon/garchomp');
+  const garchompData = await response.json();
+  console.log(garchompData);
+
+  document
+    .querySelector('body')
+    .insertAdjacentHTML(
+      'beforeend',
+      `<div><h2>${garchompData.name}</h2><span>Pokemon Info</span></div>`,
+    );
+
   // fetch is a javascript Promise-based web api that lives in the browser
   // we are chaining then methods to consume each promise as we go
-  fetch('https://pokeapi.co/api/v2/pokemon/garchomp')
-    .then(parseJson)
-    .then((garchompData) => {
-      console.log(garchompData);
-      document
-        .querySelector('body')
-        .insertAdjacentHTML(
-          'beforeend',
-          `<div><h2>${garchompData.name}</h2><span>Pokemon Info</span></div>`,
-        );
-      fetch(garchompData.species.url)
-        .then(parseJSON)
-        .then((garcompSepeciesData) => {});
-    });
+
+  // fetch('https://pokeapi.co/api/v2/pokemon/garchomp')
+  //   .then(res => res.json())
+  //   .then((garchompData) => {
+  //     console.log(garchompData);
+  //     document
+  //       .querySelector('body')
+  //       .insertAdjacentHTML(
+  //         'beforeend',
+  //         `<div><h2>${garchompData.name}</h2><span>Pokemon Info</span></div>`,
+  //       );
+  //     fetch(garchompData.species.url)
+  //       .then(parseJSON)
+  //       .then((garcompSepeciesData) => {});
+  //   });
 }
 
 document.getElementById('pokeButton').addEventListener('click', getGarchomp);
