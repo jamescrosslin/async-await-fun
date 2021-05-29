ff4bd8b3d14a35524ade1529706f3878636eea11
James Crosslin <jamesmscrosslin@gmail.com>
2021-05-29 14:12:38 -0700

    implemented dynamic html insertion

--- main.js
+++ main.js
@@ -5,14 +5,20 @@ function parseJSON(response) {
 
 // this is an arrow function, and works just like the previous one*
 //  with minor exceptions you don't need to worry about right now
-const parseIt = (response) => {
+const parseJson = (response) => {
   return response.json();
 };
 
 function getGarchomp() {
   // fetch is a javascript Promise-based web api that lives in the browser
   // we are chaining then methods to consume each promise as we go
-  fetch('https://pokeapi.co/api/v2/pokemon/garchomp').then(parseIt).then(console.log);
+  fetch('https://pokeapi.co/api/v2/pokemon/garchomp')
+    .then(parseJson)
+    .then((garchompData) => {
+      document
+        .querySelector('body')
+        .insertAdjacentHTML('beforeend', `<h2>This HTML was dynamically injected</h2>`);
+    });
 }
 
 document.getElementById('pokeButton').addEventListener('click', getGarchomp);
