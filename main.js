f0fa6b4af99fb3193691fe23832bf7f38311737b
James Crosslin <jamesmscrosslin@gmail.com>
2021-05-29 13:44:20 -0700

    added pokeapi call

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('Working!');
+// we declare a fuction that will be used as a callback
+function parseJSON(response) {
+  return response.json();
+}
+
+// this is an arrow function, and works just like the previous one*
+//  with minor exceptions you don't need to worry about right now
+const parseIt = (response) => {
+  return response.json();
+};
+
+// fetch is a javascript Promise-based web api that lives in the browser
+// we are chaining then methods to consume each promise as we go
+fetch('https://pokeapi.co/api/v2/pokemon/garchomp').then(parseIt).then(console.log);
